acd81541729157d37ed550a24cfbed63a16f4929
aaaknir <katepetrusova2012@gmail.com>
2019-07-24 20:20:49 +0300

    update 24.07 20:20

--- Task 3 - Layout authorization page/js/index.js
+++ Task 3 - Layout authorization page/js/index.js
@@ -1,24 +1,9 @@
-/**
- * 1. Use comments
- * 2. Func name `correct` is too generalized. Name funcs using verbs like `resetFormState`
- * 3. Try to use strict equal `===` instead of usual equal `==`
- * 4. It's better to use `querySelector` method to get DOM element
- * 5. Take out useful code, variables, etc.
- * 6. Use grammatically correct names, comments, etc.: write `password_line` instead of `line_password`
- * 7. Use exact selectors as you can, but without fanaticism
- * 8. Try to write together code with same logic: change `elFormMessage` properties in a row
- * 9. `correct` (`resetFormState`) method is called every click on form's input. It's better to process it's code once
- * 10. All editable data should be took out from code to global constants at the top like you do in LESS files
- **/
-
-//
 // Auth form
-//
 
 // Settings
 const validUsername = 'kate';
 const validPassword = '123';
-const nextUrlAfterSuccessSubmit = "../Task 1 - User Page/index.html";
+const nextUrlAfterSuccessSubmit = "../Task 3 - Account/index.html";
 // Submit Messages
 const msgLoginPassEmpty = 'Введите логин и пароль';
 const msgLoginEmpty = 'Введите логин';
@@ -26,21 +11,22 @@ const msgPassEmpty = 'Введите пароль';
 const msgAuthFail = 'Неправильный логин/пароль';
 // Elements
 const elLogin = document.querySelector("form#auth input#login");
-const elLoginLine = document.querySelector("form#auth div#line_login");
+const elLoginLine = document.querySelector("form#auth div#login_line");
 const elPassword = document.querySelector("form#auth input#password");
-const elPasswordLine = document.querySelector("form#auth div#line_password");
+const elPasswordLine = document.querySelector("form#auth div#password_line");
 const elFormMessage = document.querySelector("form#auth div#wrong");
 // Classes
 const clTextInputError = 'placeholder-error';
 // Functions
-function  resetFormState() {
+function  resetFormState () {
     elLogin.classList.remove(clTextInputError);
     elPassword.classList.remove(clTextInputError);
     elPasswordLine.classList.remove(clTextInputError);
     elLoginLine.classList.remove(clTextInputError);
     elFormMessage.style.visibility = "hidden";
 }
-function submitForm() {
+
+function submitForm () {
     if (!elLogin.value && !elPassword.value) {
         // Login & Pass are empty
         elFormMessage.innerHTML = msgLoginPassEmpty;
@@ -69,6 +55,5 @@ function submitForm() {
         elFormMessage.innerHTML = msgAuthFail;
         elFormMessage.style.visibility = "visible";
     }
-
     return false; // Prevent form submit
 }
