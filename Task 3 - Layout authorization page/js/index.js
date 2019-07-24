fb1e5f1d4164e8df43020c22b34fbc23750dc884
aaaknir <katepetrusova2012@gmail.com>
2019-07-24 12:33:27 +0300

    update

--- Task 3 - Layout authorization page/js/index.js
+++ Task 3 - Layout authorization page/js/index.js
@@ -1,21 +1,33 @@
-function setCustomValidaty() {
-    let login = document.getElementById("login");
-    login.addEventListener("input", function (event) {
-        if (login.validity.typeMismatch) {
-            login.setCustomValidity("Введите логин");
-        } else {
-            login.setCustomValidity("");
-        }
-    });
-
-    let password = document.getElementById("password");
-    password.addEventListener("input", function (event) {
-        if (password.validity.typeMismatch) {
-            password.setCustomValidity("Введите пароль");
-        } else {
-            password.setCustomValidity("");
-        }
-    });
+function submitClick() {
+    if (!document.getElementById("login").value && !document.getElementById("password").value) {
+        document.getElementById("wrong").innerHTML = "Введите логин и пароль";
+        document.getElementById("login").style.color ="#D51A1A";
+        document.getElementById("password").style.color = "#D51A1A";
+        document.getElementById("line_password").style.borderColor = "#D51A1A";
+        document.getElementById("line_login").style.borderColor = "#D51A1A";
+        document.getElementById("wrong").style.visibility = "visible";
+    } else if (!document.getElementById("login").value) {
+        document.getElementById("wrong").innerHTML = "Введите логин";
+        document.getElementById("login").style.color = "#D51A1A";
+        document.getElementById("line_login").style.borderColor = "#D51A1A";
+        document.getElementById("wrong").style.visibility = "visible";
+    } else if (!document.getElementById("password").value) {
+        document.getElementById("wrong").innerHTML = "Введите пароль";
+        document.getElementById("password").style.pcolor = "#D51A1A";
+        document.getElementById("line_password").style.borderColor = "#D51A1A";
+        document.getElementById("wrong").style.visibility = "visible";
+    } else if (document.getElementById("login").value == "kate" && document.getElementById("password").value == "123") {
+        window.location.href = "../Task 1 - User Page/vk.html";
+    } else {
+        document.getElementById("wrong").innerHTML = "Неправильный логин/пароль";
+        document.getElementById("wrong").style.visibility = "visible";
+    }
 }
 
-setCustomValidaty();
+function  correct() {
+    document.getElementById("login").style.color ="#CDCDCD";
+    document.getElementById("password").style.color = "#CDCDCD";
+    document.getElementById("line_password").style.borderColor = "#000000";
+    document.getElementById("line_login").style.borderColor = "#000000";
+    document.getElementById("wrong").style.visibility = "hidden";
+}
