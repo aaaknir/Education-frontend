7a1ab8f86a19b5b52d93fa53f367dda6366cccf0
aaaknir <katepetrusova2012@gmail.com>
2019-07-27 10:57:45 +0300

    Add main.js

---
+++ Task 4 - ToDo List/js/main.js
@@ -0,0 +1,27 @@
+new Vue ({
+    el: '#app',
+    data: {
+        title: 'To Do List',
+        newTodo: '',
+        todos: []
+
+    },
+    methods: {
+        addTodo () {
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = '';
+        },
+        removeTodo (todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+        allDone () {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            });
+        }
+    }
+});
