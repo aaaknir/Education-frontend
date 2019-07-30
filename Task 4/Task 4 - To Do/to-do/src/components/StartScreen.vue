4d344f02da02ccd07507a115b34ccce5bc3bd05e
aaaknir <katepetrusova2012@gmail.com>
2019-07-30 14:56:04 +0300

    code - adding filters

--- Task 4/Task 4 - To Do/to-do/src/components/StartScreen.vue
+++ Task 4/Task 4 - To Do/to-do/src/components/StartScreen.vue
@@ -1,43 +1,79 @@
 <template>
   <div class="container">
-    <div class="left">
+
+    <!-- Left column. List of lists'names -->
+    <div class="left-column">
+
+      <!-- Form of adding lists -->
       <form @submit.prevent="addList">
-        <input class="form-control-sm" v-model="newList" type="text" name="newList" placeholder="Enter your To Do List name..." id="newList">
-        <button type="submit" name="button" class="btn btn-primary">Add List</button>
+        <input maxlength="51" class="form-control-sm" v-model="newList" type="text" name="newList" placeholder="Enter your To Do list name..." id="newList"><label for="newList"></label>
+        <button type="submit" name="button" class="btn btn-primary">Add list</button>
       </form>
-      <div id="list" v-for="list in lists" :key="list.id">
-        <div id="listToDo" @click="stateList = list">
-          <span>{{list}}</span>
-          <button @click="removeList(list)" type="button" name="button" class="btn btn-danger btn-sm">Delete</button>
-          <hr>
+
+      <!-- Displaying list of lists -->
+      <div id="lists-display" v-for="list in lists" :key="list.id ">
+        <div id="list" @click="state_list=list">
+
+          <!-- Remove list -->
+          <div id="list-remove">
+            <button @click="removeList(list)" type="button" name="button" class="btn btn-danger btn-sm">&times;</button>
+          </div>
+
+          <!-- Displaying list name -->
+          <div id="list-name">
+            <span>{{list}}</span>
+          </div>
         </div>
       </div>
-      <button @click="stateList=null" type="button" name="button" class="btn btn-info btn-sm">Hide lists</button>
+
+      <!-- Hide info in right column -->
+      <button @click="state_list=null" type="button" name="button" class="btn btn-info btn-sm">Hide lists</button>
     </div>
-    <div class="right" v-if="stateList != null">
-      <span id="title_right">{{stateList.title}}</span>
+
+    <!-- Right column. Info about list, to-do list -->
+    <div class="right-column" v-if="state_list != null">
+      <span id="title-right">{{state_list.title}}</span>
+
+      <!-- Form of adding todos -->
       <form @submit.prevent="addTodo">
-        <input class="form-control-sm" v-model="newTodo" type="text" name="newTodo" placeholder="Name Todo" id="newTodo">
-        <input class="form-control-sm" v-model="newTodoDescription" type="text" name="newTodoDescription" placeholder="Describe Todo" id="newTodoDescription">
-        <input class="form-control-sm" v-model="newTodoDate" type="date" name="newTodoDate" id="newTodoDate">
-        <select class="form-control" v-model="newTodoPriority" type="" name="newTodoPriopity" id="newTodoPriopity">
-          <option>Important</option>
-          <option>Middle</option>
-          <option>None</option>
-        </select>
+        <input maxlength="51" class="form-control-sm" v-model="newTodo" type="text" name="newTodo" placeholder="Name Todo" id="newTodo"><label for="newTodo"></label>
+        <input maxlength="51" class="form-control-sm" v-model="newTodoDescription" type="text" name="newTodoDescription" placeholder="Describe Todo" id="newTodoDescription"><label for="newTodoDescription"></label>
+        <label for="newTodoDate">Deadline</label><input class="form-control-sm" v-model="newTodoDate" type="date" name="newTodoDate" id="newTodoDate">
+        <select class="form-control" v-model="newTodoPriority" type="" name="newTodoPriority" id="newTodoPriority">
+          <option @click="type = 'success'">Important</option>
+          <option @click="type = 'warning'">Middle</option>
+          <option @click="type = 'dark'">None</option>
+        </select><label for="newTodoPriority"></label>
         <button type="submit" name="button" class="btn btn-primary btn-sm">Add</button>
         <button @click="allDone" type="button" name="button" class="btn btn-success btn-sm">All done</button>
       </form>
-      <ul>
-        <li v-for="todo in lists[stateList].todos" :key="todo.id">
-          <input type="checkbox" v-model="todo.done">
-          <span id="title" :class="{done: todo.done}">{{todo.title}}</span>
-          <div id="description">{{todo.description}}</div>
-          <div id="date">{{todo.date}}</div>
-          <span class="badge badge-secondary" id="prioprity">{{todo.priority}}</span>
-          <button @click="removeTodo(todo)" type="button" name="button" class="btn btn-danger btn-sm">Remove</button>
-        </li>
-      </ul>
+
+      <!-- Filter panel -->
+      <div id="to-do-panel">
+        <button :class="{active: filter = 'all'}" @click="filter = 'all'">All</button>
+        <button :class="{active: filter = 'active'}" @click="filter = 'active'">Active</button>
+        <button :class="{active: filter = 'completed'}" @click="filter = 'completed'">Completed</button>
+        <button :class="{active: filter = 'important'}" @click="filter = 'important'">Important</button>
+        <button :class="{active: filter = 'middle'}" @click="filter = 'middle'">Middle</button>
+        <button :class="{active: filter = 'none'}" @click="filter = 'none'">None</button>
+      </div>
+
+      <!-- Info in right-column -->
+      <div id="right-info">
+        <ul>
+          <li v-for="todo in todosFiltered(state_list)" :key="todo.id">
+            <input type="checkbox" id="checkbox" v-model="todo.done"><label for="checkbox"></label>
+            <span id="title" :class="{done: todo.done}">{{todo.title}}</span><br>
+            <div id="description" :class="{done: todo.done}">{{todo.description}}</div><br>
+            <div id="date">{{todo.date}}</div>
+            <span class="badge badge-secondary" id="priority">{{todo.priority}}</span>
+            <button @click="removeTodo(todo)" type="button" name="button" class="btn btn-danger btn-sm">Remove</button>
+          </li>
+        </ul>
+      </div>
+
+      <!-- Axios -->
+      <button class="btn btn-sm btn-outline-secondary" @click="postPost">Axios</button>
     </div>
   </div>
 </template>
@@ -49,34 +85,47 @@
             return {
                 lists: [],
                 newList: '',
-                stateList: null,
                 newTodo: '',
-                todos: [],
                 newTodoDescription: '',
                 newTodoDate: '',
-                newTodoPriority: ''
+                newTodoPriority: '',
+                state_list: null,
+                filter: 'all',
+                type: 'success'
             }
         },
         methods: {
-            addList () {
+            addList() {
+                let todos = new Array(0);
                 if (this.newList) {
                     this.lists.push({
                         title: this.newList,
-                        todos: this.todos
+                        todos: todos
                     });
                     this.newList = '';
                 }
             },
-            removeList (list) {
+            todosFiltered (list) {
+                if (this.filter === 'all') {
+                    return list.todos;
+                } else if (this.filter === 'active') {
+                    return list.todos.filter(todo => !todo.done)
+                } else if (this.filter === 'completed') {
+                    return list.todos.filter(todo => todo.done)
+                }
+                return list.todos;
+            },
+            removeList(list) {
                 const listIndex = this.lists.indexOf(list);
                 this.lists.splice(listIndex, 1);
+                this.state_list = null;
             },
-            addTodo () {
+            addTodo() {
                 if (this.newTodo && this.newTodoDescription && this.newTodoDate && this.newTodoPriority) {
-                    this.lists[stateList].todos.push({
+                    this.state_list.todos.push({
                         title: this.newTodo,
                         description: this.newTodoDescription,
-                        date:this.newTodoDate,
+                        date: this.newTodoDate,
                         priority: this.newTodoPriority,
                         done: false
                     });
@@ -84,134 +133,189 @@
                     this.newTodoDescription = '';
                     this.newTodoDate = '';
                     this.newTodoPriority = '';
+                    console.log(this.type);
                 }
             },
-            removeTodo (todo) {
-                const todoIndex = this.todos.indexOf(todo);
-                this.todos.splice(todoIndex, 1);
+            removeTodo(todo) {
+                const todoIndex = this.state_list.todos.indexOf(todo);
+                this.state_list.todos.splice(todoIndex, 1);
             },
-            allDone () {
-                this.todos.forEach(todo => {
+            allDone() {
+                this.state_list.todos.forEach(todo => {
                     todo.done = true;
                 });
+            },
+            postPost() {
+                const str = JSON.stringify(this.lists);
+                axios.post('http://localhost:8081/', str)
+                    .then(response => (this.lists = response));
             }
         }
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+
   div.container {
-    float: left;
     width: 100%;
-    height: 540px;
+    height: 500px;
     font-family: 'Open Sans', sans-serif;
     color: #333333;
-    padding: 10px;
+    margin: 0 auto;
+    padding: 5px;
+    display: grid;
+    grid-template-columns: 50% 50%;
   }
 
-  div.left {
-    width: 450px;
-    float: left;
-    height: 100%;
+  div.container input {
+    outline-color: #007bff;
+  }
+
+  div.container button:hover {
+    box-shadow: 3px 3px 3px #ccc;
+  }
+
+  div.container div.left-column {
+    width: 100%;
+    height: 500px;
     overflow: auto;
     overflow-x: hidden;
   }
 
-  div.container form {
+  div.container div.left-column form {
     margin-bottom: 5px;
     background-color: rgba(205, 205, 205, 0.8);
     border-radius: 5px;
-    padding: 5px;
+    padding: 10px;
     width: 100%;
   }
 
-  div.container form input[type=text] {
-    width: 330px;
+  div.container div.left-column form input[type=text] {
+    width: calc(100% - 92px);
     margin-right: 5px;
-    outline-color: #007bff;
-  }
-  div.left #listToDo button {
-    float: right;
+    padding: 10px 18px;
   }
 
-  div.left div#listToDo {
+  div.container div.left-column div#list {
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
   }
 
-  div.right {
-    background-color: #005ab3;
-    width: 650px;
-    height: 100%;
-    float: right;
+  div.container div.left-column div#lists-display:hover {
+    background-color: rgba(187, 207, 200, 0.2);
+  }
+
+  div.container div.left-column div#list div#list-name {
     padding: 10px;
+    margin-left: 12px;
+  }
+
+  div.container div.left-column div#list div#list-remove {
+    margin-left: 14px;
+  }
+
+  div.container div.right-column {
+    width: 100%;
+    height: 500px;
+    padding: 10px 10px 10px 20px;
+    border: 2px solid #ccc;
     border-radius: 5px;
     overflow: auto;
+    overflow-x: hidden;
+    background-color: #e9e9e9;
   }
 
-  div.right span#title_right {
-    background-color: white;
+  div.container div.right-column span#title-right {
+    margin-left: 10px;
+    width: 100%;
     padding: 5px;
     border-radius: 3px;
+    font-size: 20px;
+    font-weight: bold;
   }
 
-  div.right form {
-    margin-top: 10px;
+  div.container div.right-column form {
+    margin-top: 5px;
   }
 
-  div.right form button {
-    margin: 5px 3px;
+  div.container div.right-column form button {
+    margin: 5px auto;
   }
 
-  div.right input[type=text] {
+  div.container div.right-column input[type=text] {
     margin-bottom: 3px;
-    width: 220px;
+    width: 100%;
   }
-  div.right input[type=checkbox] {
+
+  div.container div.right-column input[type=date] {
+    margin-bottom: 3px;
+    margin-left: 5px;
+  }
+
+  div.container div.right-column input[type=checkbox] {
     margin: 5px;
   }
 
-  div.right ul li {
+  div.container div.right-column div#to-do-panel {
+    margin: 5px;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  div.container div.right-column div#right-info {
+    background-color: #007bff;
+    float: left;
+    margin-bottom: 5px;
+  }
+
+  div.container div.right-column div#right-info ul li {
+    float: left;
     min-height: 10px;
-    width: 100%;
-    margin: 10px 0;
+    width: 525px;
+    margin: 10px;
     padding: 3px;
     list-style: none;
-    background-color: white;
+    background-color: rgba(255, 255, 255, 0.6);
     border-radius: 5px;
     font-family: 'Indie Flower', cursive;
     color: #333333;
     font-size: 18px;
   }
 
-  div.right ul li button {
+  div.container div.right-column div#right-info ul li button {
     margin-top: 3px;
     float: right;
   }
 
-  div.right .done {
+  div.container div.right-column .done {
     text-decoration: line-through;
   }
 
-  div.right #title {
+  div.container div.right-column div#right-info ul li #title {
     display: inline-block;
     font-weight: 700;
   }
 
-  div.right #description {
+  div.container div.right #description {
     display: inline-block;
+    width: 400px;
+    height: 30px;
   }
 
-  div.right #prioprity {
+  div.container div.right #priority {
     display: inline-block;
     font-size: 14px;
-    background-color: #dea833;
     border-radius: 3px;
     padding: 5px;
     margin-left: 5px;
     margin-right: 5px;
   }
 
-  div.right #date {
+  div.container div.right #date {
     display: inline-block;
     background-color: #8fadde;
     border-radius: 3px;
