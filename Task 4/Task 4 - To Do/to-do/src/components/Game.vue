86add20a001315d9e3b2eb06b02f7987febed8c5
aaaknir <katepetrusova2012@gmail.com>
2019-07-30 19:41:34 +0300

    add - new compomemt with interactive files

---
+++ Task 4/Task 4 - To Do/to-do/src/components/Game.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="demo">
+      <button class="btn btn-outline-info" v-on:click="show = !show">
+        Just take a rest from planning!
+      </button>
+      <transition name="fade">
+        <div v-if="show">
+          <iframe width="520"  height="300" src="https://www.youtube.com/embed/386AMmsu33c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </transition>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "AppPlanner",
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  div#demo {
+    padding: 10px;
+    display: block;
+    justify-content: space-between;
+  }
+
+  .btn {
+    margin-left: 150px;
+  }
+
+  iframe {
+    margin-left: 10px;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+</style>
--- Task 4/Task 4 - To Do/to-do/src/main.js
+++ Task 4/Task 4 - To Do/to-do/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import AppStartScreen from './components/StartScreen.vue';
+import AppGame from './components/Game.vue';
 
+Vue.component('AppGame', AppGame);
 Vue.component('AppStartScreen', AppStartScreen);
 
 new Vue({
